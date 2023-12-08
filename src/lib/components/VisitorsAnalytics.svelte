7af5deb303909d04b576886b567a92e604527315
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-08 10:49:57 +0600

    all shops analytics done

---
+++ src/lib/components/VisitorsAnalytics.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Icon from '@iconify/svelte';
+
+    let webshoptsList = [
+        {name: "ISW-Netherlands", color:"bg-[#8A30FE]"},
+        {name: "ISW-Belgium", color:"bg-[#F68F30]"},
+        {name: "ISW-Germany", color:"bg-[#04975A]"},
+        {name: "All-ISW", color:"bg-[#F34545]"},
+    ]
+</script>
+
+
+<div class="bg-white/20 rounded p-3 md:p-5 text-xs md:text-sm select-none">
+    <div class="flex items-center justify-between">
+        <div class="bg-red-500 rounded-full shadow p-2 px-3 flex items-center gap-3 font-bold">
+            All Webshops
+            <!-- <div>
+                <Icon icon="mingcute:down-fill" />
+            </div> -->
+        </div>
+        <div class="flex items-center justify-center gap-3 text-center">
+            <div>
+                <p>Visitor Today</p>
+                <p class="text-lg font-extrabold text-white">232</p>
+            </div>
+            <div>
+                <p>Total Visitors</p>
+                <p class="text-lg font-extrabold text-white">232</p>
+            </div>
+        </div>
+    </div>
+    <div class="my-5 w-full overflow-hidden">
+        <img class="w-full" src="/graph.png" alt="">
+    </div>
+    <div class="flex w-full gap-3 items-center justify-between mt-5 pt-5 w-[90%]">
+        {#each webshoptsList as {name,color}}
+        <div class="flex flex-col gap-2 items-center justify-center text-xs  opacity-80">
+            <p>{name}</p>
+            <div class={`${color} h-1 w-[80%] rounded-full`} />
+        </div>
+        {/each}
+    </div>
+</div>
