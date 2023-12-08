b41777c83bca8c84e2f8ce34417b54d33dc54dff
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-08 10:49:13 +0600

    layout cleared

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,11 @@
     })
   </script>
   
-  <div class="w-full relative max-w-[2480px] min-h-[100vh]  overflow-hidden bg-gradient-to-r from-violet-700  to-cyan-700">
+  <div class="w-full relative max-w-[2480px] min-h-[100vh]  overflow-hidden ">
     <slot />
     <!-- <div class="absolute shade-1 bg-teal-400/50 left-1 opacity-40 " /> -->
-  <div class='absolute gradient-01 opacity-50 left-0 bottom-0 ' />
+  <!-- <div class='absolute gradient-01 opacity-50 left-0 bottom-0 ' />
   <div class='absolute gradient-02 opacity-50 bottom-0 right-0 ' />
   <div class='absolute gradient-03 opacity-50 top-0 right-0 ' />
-  <div class='absolute gradient-04 opacity-50 top-0 left-0 ' />
+  <div class='absolute gradient-04 opacity-50 top-0 left-0 ' /> -->
   </div>
