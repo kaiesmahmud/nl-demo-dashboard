09fa59b6877dc3f738613798af218656c4c89eb9
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-08 10:49:30 +0600

    clear home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,31 @@
 <script>
-    import CurrentMonthRevenue from "../lib/components/CurrentMonthRevenue.svelte";
-    import Users from "../lib/components/Users.svelte";
-    import WebShops from "../lib/components/WebShops.svelte";
-    import AdminBody from "./../lib/components/admin-dashboard/AdminBody.svelte";
-    import AdminNav from "./../lib/components/admin-dashboard/AdminNav.svelte";
+    import AllShopsData from "../lib/components/AllShopsData.svelte";
+    import AllShopsTotalOrders from "../lib/components/AllShopsTotalOrders.svelte";
+    import VisitorsAnalytics from "../lib/components/VisitorsAnalytics.svelte";
 
-
-    let adminNavItems = [
-        {
-            name:"All-Products",
-            icon: "circum:shop",
-            value:"all-products"
-        },
-        {
-            name:"Add-Products",
-            icon: "material-symbols-light:add"  ,
-            value:"add-products"
-        },
-        {
-            name:"All-Users",
-            icon: "ph:users-thin",
-            value:"all-users"
-        },
-        {
-            name:"All-Orders",
-            // icon: "fluent-mdl2:reservation-orders",
-            icon: "lets-icons:order-light",
-            value:"all-orders"
-        },
-        // {
-        //     name:"All-Category",
-        //     icon: "material-symbols-light:category-outline" ,
-        //     value:"all-category"
-        // },
-
-        {
-            name:"Analytics",
-            icon: "arcticons:adobe-analytics" ,
-            value:"analytics"
-        },
-        {
-            name:"Search Engine Optimization",
-            icon: "arcticons:auditor"  ,
-            value:"seo"
-        },
-        
-    ]
 </script>
 
-<div class="p-2 md:p-5 bg-white/60 m-2 md:m-5 rounded-xl shadow min-h-[90vh]">
-    <h1 class="text-2xl md:text-3xl lg:text-4xl text-center font-light">Admin Dashboard</h1>
-    <CurrentMonthRevenue/>
-    <div class="flex w-full justify-center flex-col lg:flex-row px-2 md:px-5">
-        <WebShops/>
-        <Users/>
+<div class="p-2 md:p-4 text-white/80  rounded-xl shadow min-h-[90vh]">
+
+    <!-- =========Logo & Title Part=========== -->
+    <div class="text-2xl md:text-3xl lg:text-4xl  font-semibold flex items-center gap-2 pb-3">
+        <img class="w-10 md:w-14 " src="https://d186vdbjetg11u.cloudfront.net/static/media/logo.d6d00ab41593f9e46540095c8affeac4.svg" alt="">
+        <span class="text-red-500">TRAXNYC</span> Reports Dashboard
     </div>
-    <div>
-        
+
+    <div class="flex flex-col lg:flex-row gap-3">
+        <div class="w-full lg:w-1/3  flex flex-col gap-3">
+            <VisitorsAnalytics/>
+            <AllShopsTotalOrders/>
+        </div>
+        <div class=" w-full lg:w-2/3">
+            <AllShopsData/>
+        </div>
+
     </div>
+
+
+    
 </div>
-<!-- <div class="w-full relative flex flex-col md:flex-row gap-2 p-3 md:p-5 bg-slate-100 overflow-hidden">
-    <AdminNav {adminNavItems}  />
-    <AdminBody/>
-</div> -->
+
 
