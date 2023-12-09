3616e213a249f74e9ac50461e5be6198e8d6c06b
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-09 19:19:06 +0600

    uninstall static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,11 +10,11 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: undefined,
+			// precompress: false,
+			// strict: true
 		})
 	},
 	preprocess: vitePreprocess()
